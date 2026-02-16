2367e768f030edd946383b04f6498449fbaee761
uint128t <143170803+uint128t@users.noreply.github.com>
2026-02-17 05:20:00 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 // ==UserScript==
 // @name         Codeforces 美化
-// @namespace    http://tampermonkey.net/
+// @namespace    https://github.com/uint128t/LuoguRedirect
 // @version      7.0
 // @description  基于HSL模型保留色相反转，智能提亮文字，完美支持表格与菜单，替换页眉Logo
-// @author       You
+// @author       uint128t
 // @match        *://*.codeforces.com/*
 // @grant        GM_addStyle
 // @run-at       document-start
